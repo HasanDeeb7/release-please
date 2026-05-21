a03e30970affa259bd54e5963a0f7d8fcf5c3fb2
HasanDeeb <hasan@teelaunch.com>
2026-05-21 11:06:54 +0300

    PR Test: Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import athing from '.yalc/something'
+import athing from '.yalc/something'
 export default function Home() {
   console.log('something')
+  console.log('something')
   return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black" style={{backgroundColor: 'red'}}>
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black" style={{backgroundColor: 'red'}}>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
